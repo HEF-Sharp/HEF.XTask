a6a58e70f32cc3190ffe9029fa431e5c1a649bb0
万里涛 <wanlitao_3055601@outlook.com>
2021-05-28 16:19:04 +0800

    add rocket task message consumer

---
+++ src/HEF.XTask.RocketMQ/Consumer/RocketTaskMessageConsumer.cs
@@ -0,0 +1,36 @@
+﻿using HEF.MQ.Bus;
+using NewLife.RocketMQ.Protocol;
+using System;
+using System.Threading.Tasks;
+
+namespace HEF.XTask.RocketMQ
+{
+    public abstract class RocketTaskMessageConsumer<TMessageBody> : IMQTypedMessageConsumer<MessageExt, RocketMessage<TMessageBody>>
+    {
+        protected RocketTaskMessageConsumer(IRocketTaskScheduler rocketTaskScheduler)
+        {
+            RocketTaskScheduler = rocketTaskScheduler ?? throw new ArgumentNullException(nameof(rocketTaskScheduler));
+        }
+
+        protected IRocketTaskScheduler RocketTaskScheduler { get; }
+
+        public Task<bool> Consume(MQTypedMessage<MessageExt, RocketMessage<TMessageBody>> typedMessage)
+        {
+            var rocketMessage = typedMessage.Content;
+
+            if (rocketMessage.Delay.RemainDelaySeconds > 0)
+            {
+                //还没到延迟时间 通过剩余延迟秒数 发布新的延迟任务
+                var newRocketTask = new XRocketTask<TMessageBody>(rocketMessage.Body,
+                    rocketMessage.Dispatch.Topic, rocketMessage.Dispatch.Tag, rocketMessage.Delay.RemainDelaySeconds);
+
+                var isPublished = RocketTaskScheduler.Schedule(newRocketTask);
+                return Task.FromResult(isPublished);
+            }
+
+            return Consume(typedMessage.Message, rocketMessage.Body);
+        }
+
+        protected abstract Task<bool> Consume(MessageExt messageExt, TMessageBody messageBody);
+    }
+}
