4ee385449fc9f082686db7fd736467f8ea0c85f5
万里涛 <wanlitao_3055601@outlook.com>
2023-04-28 18:23:55 +0800

    add message xtask

---
+++ src/Common/HEF.XTask/Message/XMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace HEF.XTask
+{
+    public class XMessage<TMessageBody>
+    {
+        /// <summary>
+        /// 消息体
+        /// </summary>
+        public TMessageBody Body { get; set; }
+
+        /// <summary>
+        /// Context
+        /// </summary>
+        public XScheduleContext Context { get; set; }
+    }
+}
---
+++ src/Common/HEF.XTask/Message/XMessageTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HEF.XTask
+{
+    public class XMessageTask<TMessageBody> : XTask<XMessage<TMessageBody>>
+    {
+        #region Constructor
+        public XMessageTask(TMessageBody messageBody)
+            : base(null, BuildXMessage(messageBody))
+        { }
+
+        public XMessageTask(XMessage<TMessageBody> xMessage)
+            : base(null, ValidateXMessage(xMessage))
+        { }
+        #endregion
+
+        #region Helper Functions
+        private static XMessage<TMessageBody> BuildXMessage(TMessageBody messageBody)
+        {
+            if (messageBody == null)
+                throw new ArgumentNullException(nameof(messageBody));
+
+            return new XMessage<TMessageBody>
+            {
+                Body = messageBody
+            };
+        }
+
+        private static XMessage<TMessageBody> ValidateXMessage(XMessage<TMessageBody> xMessage)
+        {
+            if (xMessage == null)
+                throw new ArgumentNullException(nameof(xMessage));
+
+            if (xMessage.Body == null)
+                throw new ArgumentNullException($"message{nameof(xMessage.Body)}");
+
+            return xMessage;
+        }
+        #endregion
+    }
+}
